eb7b2471883b779eb91ff49459eb39e4959f0748
jeffreysaeteros <js11692@nyu.edu>
2024-04-04 23:27:35 -0400

    styles

--- App.js
+++ App.js
@@ -2,3 +2,25 @@ import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera/next';
+
+import Button from './components/Button';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+    aspectRatio: 0.8,
+  },
+  scannedDataText: {
+    color: '#fff',
+    margin: 20,
+    fontSize: 18,
+  },
+});
+
